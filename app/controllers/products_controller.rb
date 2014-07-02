5c2f3879101ac8732cb5d82388fefc8ea0d2c84b
c8rriboo <c8rriboo@gmail.com>
2014-07-02 15:34:17 +0200

    image upload

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,10 @@ class ProductsController < ApplicationController
 
 	def create
   		@product = Product.new(product_params)
+  		uploaded_io = params[:product][:photo]
+		File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+			file.write(uploaded_io.read)
+		end
   		if @product.save
   			redirect_to @product
   		else
@@ -26,7 +30,7 @@ class ProductsController < ApplicationController
 
 	def update
 		@product = Product.find(params[:id])
-		if @product.update_attributes(params[:product].permit(:name, :photo, :price, :margin, :description))
+		if @product.update_attributes(params[:product].permit(:name, params[:id], :price, :margin, :description))
 		  redirect_to products_url
 		else
 		  render "edit"
@@ -40,9 +44,13 @@ class ProductsController < ApplicationController
 		redirect_to products_url
 	end
 
+	def upload
+	  
+	end
+
 	private
 	  	def product_params
-	  		params.require(:product).permit(:name, :photo, :price,:margin, :description)
+	  		params.require(:product).permit(:name, :id, :price, :margin, :description)
 	  	end
 end
 
